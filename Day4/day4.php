0511ee978f56a6b45eae011382a159665a426d74
Manuel Rossard <manuel@rossard.fr>
2023-12-04 09:16:39 +0100

    day 4

---
+++ Day4/day4.php
@@ -0,0 +1,49 @@
+<?php
+
+$cards = array_map(
+    function ($line) {
+        [$winning, $yours] = explode('|', explode(':', str_replace('  ', ' ', $line))[1]);
+        return [
+            'winning' => array_map(fn($str) => (int)$str, explode(' ', trim($winning))),
+            'yours' => array_map(fn($str) => (int)$str, explode(' ', trim($yours))),
+        ];
+    },
+    file($argv[1], FILE_IGNORE_NEW_LINES)
+);
+
+$matches = array_map(
+    fn($card) => array_reduce(
+        $card['yours'],
+        function ($carry, $number) use ($card) {
+            return in_array($number, $card['winning'], true) ? [...$carry, $number] : $carry;
+        },
+        []
+    ),
+    $cards
+);
+
+echo 'part 1 : ', array_reduce($matches,
+    function ($carry, $card) {
+        if (count($card) === 0) {
+            return $carry;
+        }
+        return $carry + bindec('1' . str_repeat('0', count($card) - 1));
+    },
+    0
+), PHP_EOL;
+
+$countCopies = array_reduce(
+    array_keys($matches),
+    function ($carry, $cardId) use ($matches) {
+        $matchCount = count($matches[$cardId]);
+        for ($times = 0; $times < $carry[$cardId]; $times++) {
+            for ($i = 1; $i <= $matchCount; $i++) {
+                $carry[$cardId + $i] = ($carry[$cardId + $i]) + 1; //no matches on last card, don't handle the case.
+            }
+        }
+        return $carry;
+    },
+    array_map(static fn($card) => 1, $matches)
+);
+
+echo 'part 2 : ', array_sum($countCopies), PHP_EOL;
