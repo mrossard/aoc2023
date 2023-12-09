f5d743c639beb5f4f2f6cf2d16cede2787dcc11b
Manuel Rossard <manuel@rossard.fr>
2023-12-09 15:49:17 +0100

    day 9

---
+++ Day9/day9.php
@@ -0,0 +1,46 @@
+<?php
+
+$histories = array_map(
+    fn($line) => $vals = array_map(intval(...), explode(' ', $line)),
+    file($argv[1], FILE_IGNORE_NEW_LINES)
+);
+
+function printHistory($rows)
+{
+    foreach ($rows as $row) {
+        foreach ($row as $value) {
+            echo $value, ' ';
+        }
+        echo PHP_EOL;
+    }
+    echo PHP_EOL;
+}
+
+function extrapolate(array $histories, $next = true)
+{
+    foreach ($histories as $id => $history) {
+        $currentRow = 1;
+        $rows = [0 => $history];
+        //drill down
+        while (!empty(array_filter($rows[$currentRow - 1], fn($item) => $item !== 0))) {
+            for ($i = 1, $iMax = count($rows[$currentRow - 1]); $i < $iMax; $i++) {
+                $difference = $rows[$currentRow - 1][$i] - $rows[$currentRow - 1][$i - 1];
+                $rows[$currentRow][] = $difference;
+            }
+            $currentRow++;
+        }
+        //extrapolate back up
+        $rows[$currentRow - 1][] = 0;
+        for ($j = $currentRow - 2; $j >= 0; $j--) {
+            $last = array_key_last($rows[$j]);
+            $rows[$j][] = $rows[$j][$last] + $rows[$j + 1][$last];
+        }
+        $histories[$id][] = $rows[0][$last + 1];
+        //printHistory($rows);
+    }
+    return $histories;
+}
+
+echo 'part 1 : ', array_sum(array_map(fn($history) => $history[array_key_last($history)], extrapolate($histories))), PHP_EOL;
+$histories = array_map(array_reverse(...), $histories);
+echo 'part 2 : ', array_sum(array_map(fn($history) => $history[array_key_last($history)], extrapolate($histories, false))), PHP_EOL;
